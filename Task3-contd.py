4290da053b890a79ceb635a93d70038402b46213
Kelelchi Nwandu <nwandukelechi@gmail.com>
2022-04-09 13:41:56 +0100

    new

--- Task3-contd.py
+++ Task3-contd.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr  6 23:21:20 2022
+Created on Sat Apr  9 07:17:03 2022
 
 @author: NWANDU KELECHUKWU
 """
 
-#this function overall gets the list of the authors in the wikidata item from the citation
 import pywikibot
 from bs4 import BeautifulSoup
 import urllib.request
@@ -59,9 +58,9 @@ def authorname_list():
     
 authorname_list()
 
-
 #%%
-#This gets the first and last name of the author from the citation list
+
+
 def authorname_seperated():
     listt = authorname_list()
     
@@ -71,32 +70,51 @@ def authorname_seperated():
     item_dict = item.get()
     
     pli = []
+    pli1 = []
     for claim in item_dict['claims']['P50']: # Loop through items
             t_claim = claim.getTarget().title()
             pli.append(t_claim)
-    nue = pli.index('Q104918685')        
-    x1 = re.split(", ", listt[nue])
-    return x1
+    #print(pli)
+    for ID in pli:
+        nue = pli.index(ID)   
+        authors_list = listt[nue]
+        #print(authors_list)
+        x1 = re.split(", ", authors_list)
+        pli1.append(x1)
+    return pli1
+    
+    #print(x1)
+    #return x1
   
 authorname_seperated() 
 
-
 #%%
 
-def author_qualifier():
+
+def add_qualifier(data_item):
     site = pywikibot.Site("wikidata", "wikidata")
     repo = site.data_repository()
-    
-    
-    wkID = authorname_seperated()
-    claim = pywikibot.Claim(repo, u'P50') 
-    qualifier = pywikibot.Claim(repo, u'P9688')
-    qualifier.setTarget(wkID[0])
-    claim.addQualifier(qualifier, summary=u'Adding a qualifier.')
-    print('New Qualifier!')
+    item = pywikibot.ItemPage(repo, data_item) 
+    item_dict = item.get()
+    listt = []
+    dateCre = authorname_seperated() 
+    for claim in item_dict['claims']['P31']: # Loop through items
+        
+        listt.append(claim)
+    #print(listt)
+    for te, tea in zip(listt, dateCre):
+        
+        if listt.index(te) == dateCre.index(tea):
+        
+            qualifier = pywikibot.Claim(repo, u'P9688')
+            qualifier.setTarget(tea[0])
+            te.addQualifier(qualifier, summary=u'Adding a qualifier.')
+            print('New Qualifier for P9688!')
+            
+            
+            qualifier = pywikibot.Claim(repo, u'P9687')
+            qualifier.setTarget(tea[1])
+            te.addQualifier(qualifier, summary=u'Adding a qualifier.')
+            print('New Qualifier for P9687!')
 
-    claim = pywikibot.Claim(repo, u'P50') 
-    qualifier1 = pywikibot.Claim(repo, u'P9687')
-    qualifier1.setTarget(wkID[1])
-    claim.addQualifier(qualifier1, summary=u'Adding a qualifier.')
-    print('New Qualifier!!')
+add_qualifier('Q56603084')
